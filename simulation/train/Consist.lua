c65764882399daf8b94e6ea024a48d33dea13ed7
1ab0rat0ry <konturjak@gmail.com>
2024-10-08 12:12:44 +0200

    use ArrayList for vehicles in Consist

--- simulation/train/Consist.lua
+++ simulation/train/Consist.lua
@@ -3,47 +3,38 @@ local ArrayList = require "Assets/1ab0rat0ry/RWLab/utils/ArrayList.out"
 
 local logger = Logger:new(false, "BrakePipe.log")
 
-local Consist = {}
-
-Consist.length = 0
-Consist.vehicleCount = 0
-Consist.vehicles = {}
+local Consist = {
+    length = 0,
+    vehicleCount = 0,
+    vehicles = ArrayList
+}
 
 function Consist:new()
     local o = setmetatable({}, self)
     self.__index = self
-    o.vehicles = {}
+    o.vehicles = ArrayList:new()
+
     return o
 end
 
 function Consist:addVehicle(vehicle)
     self.vehicleCount = self.vehicleCount + 1
-    self.vehicles[self.vehicleCount] = vehicle
+    self.vehicles:add(vehicle)
 end
 
 function Consist:update(timeDelta)
     local simulationSteps = math.floor(60 * timeDelta)
     local simulationPartialSteps = 60 * timeDelta - simulationSteps
 
-    for i = 1, simulationSteps, 1 do
-        for k, vehicle in ipairs(self.vehicles) do
-            local nextVehicle = self.vehicles[k + 1]
-
-            if nextVehicle == nil then break end
-            vehicle.brakePipe:equalize(nextVehicle.brakePipe, timeDelta, nil, 100)
-            -- logger:info("K: "..k.." V: "..vehicle.brakePipe.pressure.." NV: "..nextVehicle.brakePipe.pressure.." NVA: "..nextVehicle.distributor.accelerationChamber.pressure)
-        end
-    end
-
-    for k, vehicle in ipairs(self.vehicles) do
-        local nextVehicle = self.vehicles[k + 1]
+    for j, vehicle in ipairs(self.vehicles:reversed()) do
+        local nextVehicle = self.vehicles.elements[self.vehicleCount - j]
 
         if nextVehicle == nil then break end
-        vehicle.brakePipe:equalize(nextVehicle.brakePipe, simulationPartialSteps, nil, 100)
-        -- logger:info("K: "..k.." V: "..vehicle.brakePipe.pressure.." NV: "..nextVehicle.brakePipe.pressure.." NVA: "..nextVehicle.distributor.accelerationChamber.pressure)
+        vehicle.brakePipe:equalize(nextVehicle.brakePipe, timeDelta, nil, 100)
+        logger:info("K: "..j.." V: "..vehicle.brakePipe.pressure.." NV: "..nextVehicle.brakePipe.pressure.." NVA: "..nextVehicle.distributor.accelerationChamber.pressure)
     end
 
-    for _, vehicle in ipairs(self.vehicles) do
+    for _, vehicle in ipairs(self.vehicles.elements) do
         vehicle:update(timeDelta)
     end
     logger:info("")
@@ -52,7 +43,7 @@ end
 function Consist:getBrakeControl()
     local brakeControlSum = 0
 
-    for _, v in ipairs(self.vehicles) do
+    for _, v in ipairs(self.vehicles.elements) do
         brakeControlSum = brakeControlSum + v:getBrakeControl()
     end
     return brakeControlSum / self.vehicleCount
