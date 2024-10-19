107a57261126d43fc83fb853090b72447d198f51
1ab0rat0ry <114067000+1ab0rat0ry@users.noreply.github.com>
2024-10-19 16:59:54 +0200

    reafactor: Vehicle.lua, Consist.lua, CD460.lua, CD063.lua

--- simulation/train/Consist.lua
+++ simulation/train/Consist.lua
@@ -1,51 +1,62 @@
-local Logger = require "Assets/1ab0rat0ry/RWLab/utils/Logger.out"
+---@type ArrayList
 local ArrayList = require "Assets/1ab0rat0ry/RWLab/utils/ArrayList.out"
 
-local logger = Logger:new(false, "BrakePipe.log")
-
+---@class Consist
+---@field public length number
+---@field public vehicleCount number
+---@field public vehicles ArrayList
 local Consist = {
     length = 0,
     vehicleCount = 0,
-    vehicles = ArrayList
+    vehicles = {}
 }
+Consist.__index = Consist
 
+---@return Consist
 function Consist:new()
-    local o = setmetatable({}, self)
-    self.__index = self
-    o.vehicles = ArrayList:new()
+    ---@type Consist
+    local obj = {
+        vehicles = ArrayList:new()
+    }
+    obj = setmetatable(obj, self)
 
-    return o
+    return obj
 end
 
+---Adds vehicle to the end.
+---@param vehicle Vehicle
 function Consist:addVehicle(vehicle)
     self.vehicleCount = self.vehicleCount + 1
     self.vehicles:add(vehicle)
 end
 
+---Updates all vehicles in consist and propagates brake pipe.
+---@param timeDelta number
 function Consist:update(timeDelta)
     local simulationSteps = math.floor(60 * timeDelta)
     local simulationPartialSteps = 60 * timeDelta - simulationSteps
 
-    for j, vehicle in ipairs(self.vehicles:reversed()) do
-        local nextVehicle = self.vehicles.elements[self.vehicleCount - j]
+    for i, vehicle in ipairs(self.vehicles:reversed()) do
+        local nextVehicle = self.vehicles.elements[self.vehicleCount - i]
 
         if nextVehicle == nil then break end
         vehicle.brakePipe:equalize(nextVehicle.brakePipe, timeDelta, nil, 100)
-        logger:info("K: "..j.." V: "..vehicle.brakePipe.pressure.." NV: "..nextVehicle.brakePipe.pressure.." NVA: "..nextVehicle.distributor.accelerationChamber.pressure)
     end
 
     for _, vehicle in ipairs(self.vehicles.elements) do
         vehicle:update(timeDelta)
     end
-    logger:info("")
 end
 
+---Calculates brake force of the whole consist.
+---@return number brake force as number between `0` and `1`
 function Consist:getBrakeControl()
     local brakeControlSum = 0
 
     for _, v in ipairs(self.vehicles.elements) do
         brakeControlSum = brakeControlSum + v:getBrakeControl()
     end
+
     return brakeControlSum / self.vehicleCount
 end
 
