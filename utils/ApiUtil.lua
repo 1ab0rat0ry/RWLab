3a0685b1d939d52ba94e1f760acd1b31b390b9d5
1ab0rat0ry <konturjak@gmail.com>
2024-11-08 14:56:32 +0100

    create local variables for Call and SysCall

--- utils/ApiUtil.lua
+++ utils/ApiUtil.lua
@@ -1,72 +1,75 @@
----Provides implementation of the game API functions in Lua.
+---Provides implementation of the game API functions.
 ---@class ApiUtil
 local ApiUtil = {}
 
+local call = Call
+local sysCall = SysCall
+
 --Script
 ---Request script to get update call once per frame.
-function ApiUtil.beginUpdate() Call("BeginUpdate") end
+function ApiUtil.beginUpdate() call("BeginUpdate") end
 
 ---	Request script to end update call once per frame.
-function ApiUtil.endUpdate() Call("EndUpdate") end
+function ApiUtil.endUpdate() call("EndUpdate") end
 
 ---@return number Integer of the simulation time in seconds.
-function ApiUtil.getSimulationTime() return Call("GetSimulationTime") end
+function ApiUtil.getSimulationTime() return call("GetSimulationTime") end
 
 ---@return number If the controls are in expert mode `1` otherwise `0`.
-function ApiUtil.isExpertMode() return Call("IsExpertMode") end
+function ApiUtil.isExpertMode() return call("IsExpertMode") end
 
 
 --Position
 ---Get the position in the current world frame of the object (local coordinates are local to a moving origin centred on the camera's current tile).
 ---@return number, number, number The position x, y, z in metres relative to the origin.
-function ApiUtil.getNearPosition() return Call("getNearPosition") end
+function ApiUtil.getNearPosition() return call("getNearPosition") end
 
 
 --Rail vehicle
 ---Is the rail vehicle controlled by the player.
 ---@return number If the train is player controlled `1`, if the train is AI controlled `0`.
-function ApiUtil.getIsPlayer() return Call("GetIsPlayer") end
+function ApiUtil.getIsPlayer() return call("GetIsPlayer") end
 
 ---Get the rail vehicle's current speed.
 ---@return number The speed in metres per second.
-function ApiUtil.getSpeed() return Call("GetSpeed") end
+function ApiUtil.getSpeed() return call("GetSpeed") end
 
 ---Get the rail vehicle's acceleration.
 ---@return number The acceleration in metres per second squared.
-function ApiUtil.getAcceleration() return Call("GetAcceleration") end
+function ApiUtil.getAcceleration() return call("GetAcceleration") end
 
 ---Get the total mass of the rail vehicle including cargo.
 ---@return number The mass in kilograms.
-function ApiUtil.getTotalMass() return Call("GetTotalMass") end
+function ApiUtil.getTotalMass() return call("GetTotalMass") end
 
 ---Get the total mass of the entire consist including cargo.
 ---@return number The mass in kilograms.
-function ApiUtil.getConsistTotalMass() return Call("GetConsistTotalMass") end
+function ApiUtil.getConsistTotalMass() return call("GetConsistTotalMass") end
 
 ---Get the consist length.
 ---@return number The length in metres.
-function ApiUtil.getConsistLength() return Call("GetConsistLength") end
+function ApiUtil.getConsistLength() return call("GetConsistLength") end
 
 ---Get the gradient at the front of the consist.
 ---@return number The gradient as a percentage.
-function ApiUtil.getGradient() return Call("GetGradient") end
+function ApiUtil.getGradient() return call("GetGradient") end
 
 ---Get the curvature (radius of curve) at the front of the consist.
 ---@return number The radius of the curve in metres.
-function ApiUtil.getCurvature() return Call("GetCurvature") end
+function ApiUtil.getCurvature() return call("GetCurvature") end
 
 ---Get the curvature relative to the front of the vehicle.
 ---@param displacement number If positive, gets curvature this number of metres ahead of the front of the vehicle. If negative, gets curvature this number of metres behind the rear of the vehicle.
 ---@return number The radius of the curve in metres `positive` if curving to the right, `negative` if curving to the left, relative to the way the vehicle is facing.
-function ApiUtil.getCurvatureAhead(displacement) return Call("GetCurvatureAhead", displacement) end
+function ApiUtil.getCurvatureAhead(displacement) return call("GetCurvatureAhead", displacement) end
 
 ---Get the rail vehicle's number.
 ---@return number The rail vehicle number.
-function ApiUtil.getRvNumber() return Call("GetRVNumber") end
+function ApiUtil.getRvNumber() return call("GetRVNumber") end
 
 ---Set the rail vehicle's number (used for changing destination boards).
 ---@param number number The new number for the vehicle.
-function ApiUtil.setRvNumber(number) Call("SetRVNumber", number) end
+function ApiUtil.setRvNumber(number) call("SetRVNumber", number) end
 
 ---Send a message to the next or previous rail vehicle in the consist.
 ---Calls the script function `OnConsistMessage(message, argument, direction)` in the next or previous rail vehicle.
@@ -75,7 +78,7 @@ function ApiUtil.setRvNumber(number) Call("SetRVNumber", number) end
 ---@param direction number Use `0` to send a message to the vehicle in front, `1` to send a message to the vehicle behind.
 ---@return number If there was a next/previous rail vehicle `1`.
 function ApiUtil.sendConsistMessage(message, argument, direction)
-    return Call("SendConsistMessage", message, argument, direction)
+    return call("SendConsistMessage", message, argument, direction)
 end
 
 ---Get the next restrictive signal's distance and state.
@@ -84,14 +87,14 @@ end
 ---@param maxDistance number Optional. How far ahead in metres to stop searching. Defaults to `10 000`.
 ---@return number, number, number, number Param 1: `1` = nothing found, `0` = end of track, `>0` = signal found; Param 2: Basic signal state: `-1` = invalid, `1` = warning, `2` = red; Param 3: Distance in metres to signal; Param4: 2D map's "pro" signal state for more detailed aspect information. `-1` = invalid, `1` = yellow, `2` = double-yellow, `3` = red, `10` = flashing-yellow, `11` = flashing-double-yellow.
 function ApiUtil.getNextRestrictiveSignal(direction, minDistance, maxDistance)
-    return Call("GetNextRestrictiveSignal", direction, minDistance, maxDistance)
+    return call("GetNextRestrictiveSignal", direction, minDistance, maxDistance)
 end
 
 ---Set a failure value on the train brake system for this vehicle.
 ---@param name string The name of the failure type. Either one of `BRAKE_FADE` (the proportion of brake power lost due to fade in the braking as a result of excess heat) or `BRAKE_LOCK` (the proportion of max force the brake is stuck at due to locking on the wheel).
 ---@param value number The value of the failure dependent on failure type.
 function ApiUtil.setBrakeFailureValue(name, value)
-    Call("SetBrakeFailureValue", name, value)
+    call("SetBrakeFailureValue", name, value)
 end
 
 ---Get the type, limit and distance to the next speed limit.
@@ -100,32 +103,32 @@ end
 ---@param maxDistance number Optional. How far ahead in metres to stop searching. Defaults to `10 000`.
 ---@return number, number, number Param 1: `-1` = nothing found, `0` = end of track, `1` = track speed limit (no signage), `2` = track speed limit sign, `3` = track speed limit; Param 2: Restriction in metres per second; Param 3: Distance in metres to speed limit.
 function ApiUtil.getNextSpeedLimit(direction, minDistance, maxDistance)
-    return Call("GetNextSpeedLimit", direction, minDistance, maxDistance)
+    return call("GetNextSpeedLimit", direction, minDistance, maxDistance)
 end
 
 ---Get the current speed limit for the consist.
 ---@param component number Optional. `0` = return current limit, `1` = return separate track and signal limit. Defaults to `0`.
 ---@return number, number If `component` is set to `0`, then a single value is returned. Otherwise, two values are returned for track and signal limits respectively.
 function ApiUtil.getCurrentSpeedLimit(component)
-    return Call("GetCurrentSpeedLimit", component)
+    return call("GetCurrentSpeedLimit", component)
 end
 
 ---Get the class of the consist.
 ---@return number eTrainTypeSpecial = `0`, eTrainTypeLightEngine = `1`, eTrainTypeExpressPassenger = `2`, eTrainTypeStoppingPassenger = `3`, eTrainTypeHighSpeedFreight = `4`, eTrainTypeExpressFreight = `5`, eTrainTypeStandardFreight = `6`, eTrainTypeLowSpeedFreight = `7`, eTrainTypeOtherFreight = `8`, eTrainTypeEmptyStock = `9`, eTrainTypeInternational = `10`
 function ApiUtil.getConsistType()
-    return Call("GetConsistType")
+    return call("GetConsistType")
 end
 
 ---Evaluates if camera is near this vehicle ( < 4 km).
 ---@return boolean
 function ApiUtil.getIsNearCamera()
-    return Call("GetIsNearCamera")
+    return call("GetIsNearCamera")
 end
 
 ---Evaluates if the vehicle is in a tunnel.
 ---@return boolean
 function ApiUtil.getIsInTunnel()
-    return Call("GetIsInTunnel")
+    return call("GetIsInTunnel")
 end
 
 
@@ -140,28 +143,28 @@ end
 ---@param name string
 ---@param index number Optional, defaults to `0`. The index of the control (usually `0` unless there are multiple controls with the same name).
 ---@return boolean If the control exists `true` otherwise `false`.
-function ApiUtil.controlExists(name, index) return Call("ControlExists", name, index or 0) end
+function ApiUtil.controlExists(name, index) return call("ControlExists", name, index or 0) end
 
 ---Get the value for a control.
 ---@param name string
 ---@param index number Optional, defaults to `0`. The index of the control (usually `0` unless there are multiple controls with the same name).
 ---@return number The value for the control.
-function ApiUtil.getControlValue(name, index) return Call("GetControlValue", name, index or 0) end
+function ApiUtil.getControlValue(name, index) return call("GetControlValue", name, index or 0) end
 
 ---Sets a value for a control.
 ---@param name string
 ---@param index number Optional, defaults to `0`. The index of the control (usually `0` unless there are multiple controls with the same name).
-function ApiUtil.setControlValue(name, value, index) Call("SetControlValue", name, index or 0, value) end
+function ApiUtil.setControlValue(name, value, index) call("SetControlValue", name, index or 0, value) end
 
 ---Locks a control so the user can no longer affect it.
 ---@param name string
 ---@param index number Optional, defaults to `0`. The index of the control (usually `0` unless there are multiple controls with the same name).
-function ApiUtil.lockControl(name, index) return Call("LockControl", name, index or 0, true) end
+function ApiUtil.lockControl(name, index) return call("LockControl", name, index or 0, true) end
 
 ---Unlocks a control so the user can affect it.
 ---@param name string
 ---@param index number Optional, defaults to `0`. The index of the control (usually `0` unless there are multiple controls with the same name).
-function ApiUtil.unlockControl(name, index) return Call("LockControl", name, index or 0, false) end
+function ApiUtil.unlockControl(name, index) return call("LockControl", name, index or 0, false) end
 
 
 --Engine
@@ -180,7 +183,7 @@ function ApiUtil.unlockControl(name, index) return Call("LockControl", name, ind
 ---@param message string
 ---@param alert number Optional, defaults to `ALERT`. The type of message box `INFO(0)` or `ALERT(1)`.
 function ApiUtil.showMessage(title, message, alert)
-    SysCall("ScenarioManager:ShowMessage", title, message, alert or 1)
+    sysCall("ScenarioManager:ShowMessage", title, message, alert or 1)
 end
 
 ---Shows an info dialogue box with a message and extended attributes.
@@ -192,7 +195,7 @@ end
 ---@param size number The size of the message box: `MSG_SMALL(0)`, `MSG_REG(1)`, `MSG_LRG(2)`.
 ---@param pause boolean If `true` pause the game while the message is shown.
 function ApiUtil.showInfoMessageExt(title, message, time, pos, size, pause)
-    SysCall("ScenarioManager:ShowInfoMessageExt", title, message, time, pos, size, pause)
+    sysCall("ScenarioManager:ShowInfoMessageExt", title, message, time, pos, size, pause)
 end
 
 ---Shows an alert dialogue box with a message and extended attributes.
@@ -202,7 +205,7 @@ end
 ---@param time number The time to show the message, set to `0` for indefinite.
 ---@param event string Event name triggered on click of message.
 function ApiUtil.showAlertMessageExt(title, message, time, event)
-    SysCall("ScenarioManager:ShowAlertMessageExt", title, message, time, event)
+    sysCall("ScenarioManager:ShowAlertMessageExt", title, message, time, event)
 end
 
 
