eeaaa89314c6b0e0f210c58093f52d040e630216
1ab0rat0ry <114067000+1ab0rat0ry@users.noreply.github.com>
2024-11-18 20:44:57 +0100

    new air flow calculation

--- simulation/brake/common/AirFlow.lua
+++ simulation/brake/common/AirFlow.lua
@@ -1,8 +1,14 @@
-local ATM_PRESSURE = 101325 --Pa
-local AIR_DENSITY = 1.204 -- kg/m^3, at 20°C and 101 325 Pa
+local ATM_PRESSURE = 101325
+local AIR_DENSITY = 1.204
+local AIR_SPECIFIC_GAS_CONSTANT = 287.053
 local HEAT_RATIO = 1.4
+local HEAT_RATIO_INC = HEAT_RATIO + 1
+local HEAT_RATIO_DEC = HEAT_RATIO - 1
+local CRITICAL_PRESSURE_RATIO = (2 / HEAT_RATIO_INC) ^ (HEAT_RATIO / HEAT_RATIO_DEC)
+local CORRECTION_COEF = 0.7
 local CELSIUS_TO_KELVIN = 273.15
 local BAR_TO_PASCAL = 1e5
+local CUBIC_METRES_TO_LITRES = 1000
 
 local DEBUG = false
 local Logger = require "Assets/1ab0rat0ry/RWLab/utils/Logger.out"
@@ -12,32 +18,35 @@ local logger = Logger:new(DEBUG, "!Flow.log")
 ---@class AirFlow
 local AirFlow = {}
 
+---`Source:` https://en.wikipedia.org/wiki/Orifice_plate#Compressible_flow and
+---https://en.wikipedia.org/wiki/Choked_flow#Choking_in_change_of_cross_section_flow
+---@param pressure1 number
+---@param pressure2 number
+---@param area number
+---@param temperature number
+---@return number
 function AirFlow:get(pressure1, pressure2, area, temperature)
-    if pressure2 > pressure1 then return self:get(pressure2, pressure1, area, temperature) end
-    pressure1 = pressure1 * BAR_TO_PASCAL + ATM_PRESSURE
-    pressure2 = pressure2 * BAR_TO_PASCAL + ATM_PRESSURE
+    local pressureIn = math.max(pressure1, pressure2) * BAR_TO_PASCAL + ATM_PRESSURE
+    local pressureOut = math.min(pressure1, pressure2) * BAR_TO_PASCAL + ATM_PRESSURE
+    local pressureRatio = pressureOut / pressureIn
+    local baseComponent = CORRECTION_COEF * area / AIR_DENSITY * CUBIC_METRES_TO_LITRES
 
-    local pressureRatio = pressure2 / pressure1;
-    local heatRatioInc = HEAT_RATIO + 1
-    local heatRatioDec = HEAT_RATIO - 1
-    local threshold = (2 * HEAT_RATIO / heatRatioInc) ^ (HEAT_RATIO / heatRatioDec)
-    local baseComponent = pressure1 * area / self:getSpeedOfSound(temperature) / AIR_DENSITY
+    if pressureRatio > CRITICAL_PRESSURE_RATIO then
+        local component1 = 2 * self:getDensity(pressureIn, temperature) * pressureIn * HEAT_RATIO / HEAT_RATIO_DEC
+        local component2 = pressureRatio ^ (2 / HEAT_RATIO) - pressureRatio ^ (HEAT_RATIO_INC / HEAT_RATIO)
 
-    if pressureRatio > threshold then
-        local component1 = (2 * HEAT_RATIO ^ 2) / heatRatioDec
-        local component2 = pressureRatio ^ (2 / HEAT_RATIO)
-        local component3 = 1 - pressureRatio ^ (heatRatioDec / HEAT_RATIO)
-        logger:info("Subsonic P1: "..pressure1.." P2: "..pressure2.." R: "..pressureRatio.." T: "..threshold.." Flow: "..baseComponent * math.sqrt(component1 * component2 * component3))
-
-        return baseComponent * math.sqrt(component1 * component2 * component3)
+        return baseComponent * math.sqrt(component1 * component2)
     else
-        logger:info("Choked P1: "..pressure1.." P2: "..pressure2.." R: "..pressureRatio.." T: "..threshold.." Flow: "..baseComponent * HEAT_RATIO * (2 / heatRatioInc) ^ (heatRatioInc / (2 * heatRatioDec)))
-        return baseComponent * HEAT_RATIO * (2 / heatRatioInc) ^ (heatRatioInc / (2 * heatRatioDec))
+        return baseComponent * math.sqrt(HEAT_RATIO * self:getDensity(pressureIn, temperature) * pressureIn * CRITICAL_PRESSURE_RATIO)
     end
 end
 
 function AirFlow:getSpeedOfSound(temperature)
     return math.sqrt(1 + temperature / CELSIUS_TO_KELVIN)
+end
+
+function AirFlow:getDensity(pressure, temperature)
+    return pressure / (AIR_SPECIFIC_GAS_CONSTANT * (temperature + CELSIUS_TO_KELVIN))
 end
 
 return AirFlow
